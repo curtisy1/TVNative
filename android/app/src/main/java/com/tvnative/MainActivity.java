c29d698a9e50f14ffde37b2d4253b5030c9aee1b
curtisy <lausklaus44@gmail.com>
2019-11-17 14:57:33 +0100

    Use react-native-navigation and update to RN > 0.60

--- android/app/src/main/java/com/tvnative/MainActivity.java
+++ android/app/src/main/java/com/tvnative/MainActivity.java
@@ -1,9 +1,7 @@
 package com.tvnative;
 
 import com.reactnativenavigation.NavigationActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+// import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends NavigationActivity {
     // @Override
--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,7 @@ import MainView from "./components/MainView";
 import Sidemenu from "./components/Sidemenu";
 import Settings from "./components/Settings";
 
-Navigation.registerComponent(`TvNative.MainMainView`, () => MainView);
+Navigation.registerComponent(`TvNative.MainView`, () => MainView);
 Navigation.registerComponent(`TvNative.Sidemenu`, () => Sidemenu);
 Navigation.registerComponent(`TvNative.Settings`, () => Settings);
 
